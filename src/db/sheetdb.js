e2d997c3f45c24fef4630c5642c17b399d956b82
tacowhisperer <chato94@gmail.com>
2020-06-01 13:01:56 -0500

    Finished implementing and documenting sheetdb.js

--- src/db/sheetdb.js
+++ src/db/sheetdb.js
@@ -6,14 +6,13 @@ function sheetdbFactory() {
 }
 
 /**
- * 
+ * Sheet DB Object. Used to interface between a specific Sheet database implementation and a different, non-related
+ * module. It aids navigating a database shaped like a spreadsheet.
  */
 function sheetdbObject() {
 	// Communication adapter for receiving data from other modules.
 	var commAdapter = {
 		getSheetObject: () => Object.create({}),
-		getPageHeight: () => 0,
-		getPageWidth: () => 0,
 		getSheetContent: (sheetObject, page, fromX, toX, fromY, toY) => []
 	};
 
@@ -40,18 +39,39 @@ function sheetdbObject() {
 		return this;
 	};
 
+	/**
+	 * Reads the rows of the sheet provided by the adapter at the specified row index range.
+	 * @param {string} page The page name of the sheet to read.
+	 * @param {number} from The index of the starting row to read.
+	 * @param {number} to The index of the ending row to read (non-inclusive).
+	 * @returns {Promise} The data obtained from the sheet.
+	 */
 	this.readSheetRows = async function(page, from, to) {
-		const a = commAdapter;
-		return a.getSheetContent(a.getSheetObject(), page, from, to, 0, a.getPageWidth());
+		return await this.readSquare(page, from, to, 0, Infinity);
 	};
 
+	/**
+	 * Reads the columns of the sheet provided by the adapter at the specified colunm index range.
+	 * @param {string} page The page name of the sheet to read.
+	 * @param {number} from The index of the starting column to read.
+	 * @param {number} to The index of the ending column to read (non-inclusive).
+	 * @returns {Promise} The data obtained from the sheet.
+	 */
 	this.readSheetColumns = async function(page, from, to) {
-		const a = commAdapter;
-		return a.getSheetContent(a.getSheetObject(), page, 0, a.getPageHeight(), from, to);
+		return await this.readSquare(page, 0, Infinity, from, to);
 	};
 
+	/**
+	 * Reads the data of the sheet provided by the adapter at the specified row/column index range.
+	 * @param {string} page The page name of the sheet to read.
+	 * @param {number} fromX The index of the starting row to read.
+	 * @param {number} toX The index of the ending row to read (non-inclusive).
+	 * @param {number} fromY The index of the starting column to read.
+	 * @param {number} toY The index of the ending column to read (non-inclusive).
+	 * @returns {Promise} The data obtained from the sheet.
+	 */
 	this.readSquare = async function(page, fromX, toX, fromY, toY) {
-		return 
+		return commAdapter.getSheetContent(commAdapter.getSheetObject(), page, fromX, toX, fromY, toY);
 	};
 
 	/**
