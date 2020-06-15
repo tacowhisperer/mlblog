198eca8df5f343b144ff5680f73cb93962087a1c
tacowhisperer <chato94@gmail.com>
2020-06-15 11:53:07 -0500

    Downloaded link assets.

---
+++ www/src/view/component.js
@@ -0,0 +1,19 @@
+/**
+ * @author tacowhisperer
+ */
+
+/**
+ * Top level tokens defined in all submodules that inherit from the Component class.
+ */
+const TOKENS = {
+	COMPONENT: /\{\{.*?\}\}/gi
+};
+
+/**
+ * Top level class that encompasses the Component.js framework for dynamically building HTML views without
+ * the need to explicitly hard-code everything into an HTML file.
+ * @param {string} markup The HTML-like markup string that should be compiled into a workable HTML object.
+ */
+function Component(markup) {
+
+}
